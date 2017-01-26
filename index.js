566718d6daa93b4c6152104ccc319bad60b98b4c
IyarMadmon <madmon18@gmail.com>
2017-01-26 21:45:00 +0200

    ignore module folder

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
-var MongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
+// var MongoClient = require('mongodb').MongoClient
+//   , assert = require('assert');
+//
+// // Connection URL
+// var url = 'mongodb://localhost:27017/gym';
+// // Use connect method to connect to the server
+// MongoClient.connect(url, function(err, db) {
+//   assert.equal(null, err);
+//   console.log("Connected successfully to server");
+//
+//   insertDocuments(db, function() {
+//     db.close();
+//   });
+// });
+//
+//
+// var insertDocuments = function(db, callback) {
+//   // Get the documents collection
+//   var collection = db.collection('something');
+//   // Insert some documents
+//   collection.save({"names": ["itzk", "dotan"], "surname": "kabali", "mz": "201271269"} , function(err, result) {
+//     assert.equal(err, null);
+//     console.log("Inserted 3 documents into the collection");
+//     callback(result);
+//   });
+// };
+var http = require('http');
 
-// Connection URL
-var url = 'mongodb://localhost:27017/gym';
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-
-  insertDocuments(db, function() {
-    db.close();
-  });
-});
-
-
-var insertDocuments = function(db, callback) {
-  // Get the documents collection
-  var collection = db.collection('something');
-  // Insert some documents
-  collection.save({"names": ["itzk", "dotan"], "surname": "kabali", "mz": "201271269"} , function(err, result) {
-    assert.equal(err, null);
-    console.log("Inserted 3 documents into the collection");
-    callback(result);
-  });
-};
+http.createServer(function(request, response) {
+  response.writeHead(200);
+  response.write("Hello, this is Shoham");
+  console.log("Yes!!!");
+  response.end();
+}).listen(8080);
