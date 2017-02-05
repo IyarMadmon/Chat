892b04218a6a60aef8638daaaa1fdaff632c1d5c
IyarMadmon <madmon18@gmail.com>
2017-02-05 19:22:17 +0200

    dfsdfsd

--- static/js/main.js
+++ static/js/main.js
@@ -1,39 +1,122 @@
-(function(){
+class ChatBox extends React.Component {
 
-  const socket = io();
-  let currentRoom;
+  constructor() {
+    super();
+    this.state = {
+      selectedRoom: null,
+      chatContent: "welcome",
+      userName: ""
+    };
+  };
 
-  socket.on('connected', function (data) {
-       console.log("data = ", data);
+  componentDidMount() {
+    socket.on('connected', (data) => {
+      console.log(data);
+    });
+    socket.on('disconnect', () => {
+      if(this.state.selectedRoom) {
+        socket.emit("unSubscribeFromRoom", currentRoom);
+      }
+      console.log('disconnect');
+    });
+    socket.on('newMessage', (data) => {
+      console.log("newMessage = ", data);
+      const currentText = this.state.chatContent;
+      this.setState({chatContent: currentText + `<strong> ${data.userName} </strong>:  ${data.messageContent} <br>`});
+    });
+  }
 
-       $("#roomSelector").change(function ()  {
-         roomVal = $(this).val();
-         if(!currentRoom) {
-           socket.emit("subscribeToRoom", roomVal);
-         } else if(currentRoom !== roomVal) {
-           socket.emit("unSubscribeFromRoom", currentRoom);
-           socket.emit("subscribeToRoom", roomVal);
-           $("#chat").html("");
-         }
-         currentRoom = roomVal;
-       });
+  render() {
+    return (<div>
+              <UserName onChange={this._onChangeUserName.bind(this)}/>
+              <RoomSelector rooms={this._getRooms()} onChange={this._selectRoom.bind(this)}/>
+              <Chat chatContent={this.state.chatContent}/>
+              <MessageInputAndButton enabled={this.state.selectedRoom && this.state.userName !== ""} onSubmit={this._onSendMessage.bind(this)}/>
+            </div>);
+  }
 
-       $("#sendMessage").click(function () {
-         const username = $("#username").val();
-         const message = $("#message").val();
-         const roomVal = $("#roomSelector").val();
-         socket.emit("newMessage", {'username': username, 'messageContent': message, 'room':roomVal});
-         $("#message").val("");
-       });
+  _onSendMessage(messageContent) {
+    socket.emit("newMessage", {'userName': this.state.userName, 'messageContent': messageContent, room:this.state.selectedRoom});
+  }
 
-       socket.on('newMessage', (data) => {
-         console.log("newMessage = ", data);
-         $("#chat").html($("#chat").html() + "<strong>" + data.username + "</strong>: " + data.messageContent + "<br>");
-       });
+  _onChangeUserName(event) {
+    this.setState({userName:event.target.value});
+  }
 
-       socket.on('disconnect', function() {
-         socket.emit("unSubscribeFromRoom", currentRoom);
-         console.log('disconnect');
-       });
-  });
-})();
+  _selectRoom(event) {
+    if(this.state.selectedRoom) {
+      socket.emit("unSubscribeFromRoom", this.state.selectedRoom);
+    }
+    this.setState({selectedRoom:event.target.value, chatContent:`Welcome to room ${event.target.value} <br>`}, () => console.log(this.state.chatContent));
+
+    this.setState({selectedRoom:event.target.value}, () => console.log(this.state.selectedRoom));
+    socket.emit("subscribeToRoom", event.target.value);
+  }
+
+  _getRooms() {
+    return [{id:1, name:"North"}, {id:2, name:"South"}];
+  }
+}
+
+function UserName(props) {
+  return (<input type="text" id="username" placeholder="Enter nickname" onChange={props.onChange}></input>);
+}
+
+function RoomSelector(props) {
+  return (<span>
+            <span>room:</span>
+            <select id = "roomSelector" onChange={props.onChange}>
+              <option disabled selected value=""> -- select a room -- </option>
+              {props.rooms.map(room => <RoomSelectOption id={room.id} name={room.name} key={room.id}>{room.name}</RoomSelectOption>)}
+            </select>
+          </span>);
+}
+
+function RoomSelectOption(props) {
+  return (<option value={props.name} id={props.id}>{props.name}</option>);
+}
+
+class Chat extends React.Component {
+
+  createMarkup() {
+    return {__html: this.props.chatContent};
+  }
+
+  render() {
+    return (<div id="chat" dangerouslySetInnerHTML={this.createMarkup()}></div>);
+  };
+}
+
+class MessageInputAndButton extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue : ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    if(this.state.inputValue === "") return;
+    this.props.onSubmit(this.state.inputValue);
+    this.setState({inputValue : ""});
+  }
+
+  handleChange(event) {
+    this.setState({inputValue: event.target.value});
+  }
+
+  render() {
+    return (<form onSubmit={this.handleSubmit}>
+              <input disabled={!this.props.enabled} type="text" id="message" placeholder="Enter message" value={this.state.inputValue} onChange={this.handleChange}/>
+              <input id="sendMessage" type="submit" value="->"/>
+            </form>);
+  }
+}
+
+const socket = io();
+ReactDOM.render( <ChatBox/>, document.getElementById("main"));
