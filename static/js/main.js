eea3d125034bf1f2a4d3e22deabe4adb29ba2f0e
IyarMadmon <madmon18@gmail.com>
2017-02-12 22:11:13 +0200

    shit cleared. moving to redux now

--- static/js/main.js
+++ static/js/main.js
@@ -1,36 +1,9 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import { Link, Router, Route, IndexRoute, hashHistory } from "react-router";
 import ChatBox from './components/chatBox';
 import Login from './components/Login';
 
 const app = document.getElementById("app");
 
-class App extends React.Component {
-
-
-  render() {
-    return (<div>
-                {this.props.children}
-              </div>);
-  }
-}
-
-function requireAuth(nextState, replaceState) {
-  console.log("Iyar", new Date().getTime());
-  // replaceState({ nextPathname: nextState.location.pathname }, '/login');
-  // replace({
-  //    pathname: '/login',
-  //    state: { nextPathname: nextState.location.pathname }
-  //  });
-  this.context.router.replace('login');
-}
-
 ReactDOM.render(
-  <Router history={hashHistory}>
-    <Route path="/" component={App} >
-      <Route path ="Login" name ="Login" component={Login}></Route>
-      <IndexRoute onEnter={requireAuth.bind(this)} component={ChatBox}></IndexRoute>
-    </Route>
-  </Router>,
-app);
+  <ChatBox/>, app);
