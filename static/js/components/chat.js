ac11a3a10530b0197c7c71fe596b85a53c804387
IyarMadmon <madmon18@gmail.com>
2017-02-14 21:01:26 +0200

    oops

--- static/js/components/chat.js
+++ static/js/components/chat.js
@@ -1,18 +1,24 @@
 import React from 'react';
 
 export default class Chat extends React.Component {
-
-  createMarkup() {
-    return {__html: this.props.chatContent};
-  }
-
   render() {
     return (<div>
               <h4>Hello {this.props.userName}</h4>
-              <div
-                id="chat"
-                dangerouslySetInnerHTML={this.createMarkup()}>
+              <div id="chat">
+                {this.props.chatContent.map(message => <MessageRow
+                                                          sender={message.sender}
+                                                          content={message.content}
+                                                          key={message.id}
+                                                        >
+                                                       </MessageRow>)}
               </div>
             </div>);
   };
+}
+
+function MessageRow(props) {
+  return (<div>
+            <strong>{props.sender}: </strong>
+            <span>{props.content}</span>
+          </div>);
 }
