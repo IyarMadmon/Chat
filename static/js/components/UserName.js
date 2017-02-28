e72e07da332452a606f43901a6f52e0ac9e561df
IyarMadmon <madmon18@gmail.com>
2017-02-28 22:21:31 +0200

    modal ruin

--- static/js/components/UserName.js
+++ static/js/components/UserName.js
@@ -34,31 +34,8 @@ export default class UserName extends React.Component {
       isOpen={this.props.isModalOpen}
       shouldCloseOnOverlayClick={false}
       contentLabel="No Overlay Click Modal"
-      style={{
-        overlay : {
-          position          : 'fixed',
-          top               : 0,
-          left              : 0,
-          right             : 0,
-          bottom            : 0,
-          backgroundColor   : 'rgba(255, 255, 255, 0.75)'
-        },
-        content : {
-          position                   : 'absolute',
-          top                        : '200px',
-          left                       : '200px',
-          right                      : '200px',
-          bottom                     : '200px',
-          border                     : '1px solid #ccc',
-          background                 : '#fff',
-          overflow                   : 'auto',
-          WebkitOverflowScrolling    : 'touch',
-          borderRadius               : '4px',
-          outline                    : 'none',
-          padding                    : '20px'
-
-        }
-      }}
+      className="modal-content"
+      overlayClassName="modal-overlay"
     >
       <form onSubmit={this._onHandleSubmit}>
         <input
