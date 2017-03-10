95bf9469e346a4cb6608d1a7e4e94904343dccf1
IyarMadmon <madmon18@gmail.com>
2017-03-10 22:11:42 +0200

    now flexbox

--- static/js/components/Header.js
+++ static/js/components/Header.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
 export default class Header extends React.Component {
+
+  displayName() {
+    if(this.props.userName) {
+        return `Hello ${this.props.userName}`;
+    }
+    return '';
+
+  }
   render() {
     return (<header >
-              <h4 id="name-display">Hello {this.props.userName}</h4>
+              <h4 id="name-display">{this.displayName()}</h4>
               <h1> Chat Shit </h1>
             </header>);
   }
